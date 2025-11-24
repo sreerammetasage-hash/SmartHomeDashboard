8a3769424976427f7218277ce27fb1aa8a20718c
sreerammetasage-hash <sreeram.metasage@gmail.com>
2025-11-24 11:36:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Dashboard Loaded!");
+
+// Dummy switches for UI
+document.getElementById("fanBtn").onclick = () => alert("Fan toggle pressed!");
+document.getElementById("lightBtn").onclick = () => alert("Light toggle pressed!");
+document.getElementById("pirLightBtn").onclick = () => alert("PIR Light toggle pressed!");
+document.getElementById("modeToggle").onclick = () => alert("Mode toggle pressed!");
